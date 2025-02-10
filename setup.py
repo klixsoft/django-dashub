f881f1fc6397eaa7b675baf2659b6f134d33a3ed
Suresh Chand <scthakuri12a@gmail.com>
2025-02-10 21:04:58 +0545

    Change version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="django_dashub",
-    version="1.0",
+    version="1.0.1",
     author="Suresh Chand",
     author_email="scthakuri12a@gmail.com",
     description="A modern Django admin dashboard with enhanced customization options, inspired by Jazzmin but featuring a fresh theme and additional functionality.",
