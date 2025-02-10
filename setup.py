496958f4de5154e356bc46c7bb3175449f2bf8e8
Suresh Chand <scthakuri12a@gmail.com>
2025-02-10 20:53:42 +0545

    fix installation setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     ],
     include_package_data=True,
     package_data={
-        "django_filehub": [
+        "django_dashub": [
             "templates/*",
             "static/*",
             "templatetags/*",
